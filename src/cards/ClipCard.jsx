d05b7e276a0c07d3c24151fcb961311a0a55bf9e
ionlizarazu <ilizarazu@codesyntax.com>
2022-03-01 17:00:54 +0100

    kliparen txartelaren formatua aldatu

--- src/cards/ClipCard.jsx
+++ src/cards/ClipCard.jsx
@@ -6,12 +6,6 @@ import moment from 'moment';
 const ClipCard = ({ clip }) => {
   return (
     <Card>
-      <Card.Content>
-        <Label attached="top left" color="violet">
-          {clip.broadcaster_name}
-        </Label>
-        <Card.Header>{clip.title}</Card.Header>
-      </Card.Content>
       <a href={clip.url} target="_blank" rel="noreferrer">
         <Image
           src={clip.thumbnail_url
@@ -19,71 +13,80 @@ const ClipCard = ({ clip }) => {
             .replace('%{height}', 228)}
         />
       </a>
+      <Card.Content>
+        <Card.Header>{clip.title}</Card.Header>
+        <Card.Description>
+          <Grid>
+            <Grid.Row columns={2}>
+              <Grid.Column width={12}>
+                <div>
+                  <Icon name="clock" />
+                  {moment(clip.created_at).format('YYYY/MM/DD')}
+                </div>
+                <div>
+                  <Icon name="paperclip" />
+                  {clip.creator_name}
+                </div>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <a
+                  type="button"
+                  role="button"
+                  title="Share on twitter"
+                  target={'_blank'}
+                  rel="noreferrer"
+                  href={
+                    'https://twitter.com/intent/tweet?url=' +
+                    '[' +
+                    clip.broadcaster_name +
+                    ']' +
+                    '%0A' +
+                    clip.title +
+                    '%0A%0A' +
+                    clip.url
+                  }
+                >
+                  <Icon name="twitter" color="blue" size="big" />
+                </a>
+                <br />
+                <Popup
+                  content="Arbelean kopiatu da zure sareetan partekatzeko!"
+                  hideOnScroll
+                  basic
+                  inverted
+                  position="top center"
+                  on="click"
+                  trigger={
+                    <Icon
+                      name="copy"
+                      size="big"
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          '[' +
+                            clip.broadcaster_name +
+                            ']' +
+                            '\n' +
+                            clip.title +
+                            '\n\n' +
+                            clip.url,
+                        );
+                      }}
+                    />
+                  }
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Card.Description>
+        <Label attached="top left" color="violet">
+          {clip.broadcaster_name}
+        </Label>
+      </Card.Content>
       <Card.Content extra>
-        <Grid divided="vertically">
-          <Grid.Row columns={2}>
-            <Grid.Column>
-              <div>
-                <Icon name="eye" />
-                {clip.view_count} ikustaldi
-              </div>
-              <div>
-                <Icon name="clock" />
-                {moment(clip.created_at).format('YYYY/MM/DD')}
-              </div>
-              <div>
-                <Icon name="paperclip" />
-                {clip.creator_name}
-              </div>
-            </Grid.Column>
-            <Grid.Column>
-              <a
-                type="button"
-                role="button"
-                title="Share on twitter"
-                target={'_blank'}
-                rel="noreferrer"
-                href={
-                  'https://twitter.com/intent/tweet?url=' +
-                  '[' +
-                  clip.broadcaster_name +
-                  ']' +
-                  '%0A' +
-                  clip.title +
-                  '%0A%0A' +
-                  clip.url
-                }
-              >
-                <Icon name="twitter" color="blue" size="big" />
-              </a>
-              <Popup
-                content="Arbelean kopiatu da zure sareetan partekatzeko!"
-                hideOnScroll
-                basic
-                inverted
-                position="top center"
-                on="click"
-                trigger={
-                  <Icon
-                    name="copy"
-                    size="big"
-                    onClick={() => {
-                      navigator.clipboard.writeText(
-                        '[' +
-                          clip.broadcaster_name +
-                          ']' +
-                          '\n' +
-                          clip.title +
-                          '\n\n' +
-                          clip.url,
-                      );
-                    }}
-                  />
-                }
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <div>
+          <Icon name="eye" />
+          {clip.view_count} ikustaldi
+        </div>
       </Card.Content>
     </Card>
   );
