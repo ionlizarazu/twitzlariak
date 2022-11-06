b0fc8a786fcca59b5034354b329fd7be01ead733
ionlizarazu <ilizarazu@codesyntax.com>
2022-11-06 01:21:31 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Menu } from 'semantic-ui-react';
 import './header.css';
-
+import { useLocation } from 'react-router-dom';
 export const Header = () => {
+  const location = useLocation();
+  React.useEffect(() => {
+    console.log(location);
+  }, [location.pathname]);
   return (
     <div className="header">
+      {location.pathname === '/' && <h1>Twitch euskaraz</h1>}
       <Menu inverted>
         <Container>
           <Menu.Item as={Link} to="/">
